0d76474e04a4b688c55fdfcfdf9d0b72148479bd
matheuslongo <longo.matheusmacedo@gmail.com>
2022-09-16 10:40:34 -0300

    Adicionando a api no git

---
+++ src/main/java/helpDesck/HelpDesckApplication.java
@@ -0,0 +1,13 @@
+package helpDesck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDesckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDesckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/helpDesck/HelpDesckApplicationTests.java
@@ -0,0 +1,13 @@
+package helpDesck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDesckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
